a875656b06fa47c4364a8c1be044e9ef3cbf2daf
hyenaenpress <catnaro16@gmail.com>
2020-08-23 14:16:46 +0900

    산술연산자

---
+++ hello.py
@@ -0,0 +1,40 @@
+# coding =utf-8
+print('안녕하세요')
+# 이것이 코멘트. 주석을 다는 것
+"""
+여러줄 코멘트 입니다 여러줄 주석
+변수이름 앞에 숫자 들어갈 수 없음 대소문자 구분함
+예약어 
+"""
+name = "amy"
+print(name)
+
+_2Myname = "김씨"
+print(_2Myname)
+
+"""
+문자를 곱하기 할 수 있음
+"""
+name2 =name*2
+print(name2)
+
+"""
+자료형 데이터 타입
+Number 숫자 , 2. String 문자열 3. List 4 Tuples 튜플 5. Dictionary
+arithmetic operator
++
+- 배기 
+* 곱하기
+/ 나눗셈
+// 자리내림 나눗셈 
+% 나머지 구하는 연산자
+** 거듭제곱 연산자
+산술연산자 우선 순위 괄호가 먼저 실행 된다. 
+"""
+print("7+2=",7+2)
+print("7+2=",7-2)
+print("7+2=",7*2)
+print("7+2=",7+2)
+print("7+2=",7+2)
+print("7+2=",7+2)
+
