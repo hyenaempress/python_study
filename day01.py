ba056758ce5406c9dbe901f37a83ad456e597633
hyenaenpress <catnaro16@gmail.com>
2020-08-23 16:01:45 +0900

    산술연산자

---
+++ day01.py
@@ -0,0 +1,11 @@
+
+#문자열 관련된 것
+qu= "etre ou ne pas etre"
+m_qu= ''' telle
+est las qustion'''
+new_striong =qu + m_qu
+"""줄바꿈"""
+print('\n'*5)
+print(new_striong)
+"""이놈은 왜 꼭 불어로 예제를 만드는거지? 단따옴표 안에 쌍따옴표 있으면 출력이 될 수 있고 출력을 해 줄 수 있다."""
+
---
+++ day01/Number.py
@@ -0,0 +1,22 @@
+"""integer 정수형 floating-point 실수형 Octal 8진수 Hexadecimal 16진수 Complex number 복소수"""
+
+"""integer 정수형 입력 방법"""
+a=123
+b=233
+
+"""floating-point 실수형 입력 방법"""
+
+a=1.2
+b=2.3
+
+"""복소수 입력 방법 e 를 사용한다"""
+a= 4.24E10
+a= 4.24e10
+
+"""8진수 입력방법 0o를 사용한다"""
+
+a= 0o177
+
+"""16진수 입력 방법 0x 를 사용한다"""
+a=0x8ff
+
